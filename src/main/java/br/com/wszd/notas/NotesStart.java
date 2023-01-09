3bdee9cb3f65ea54d35fd52bd5bec71397782b0d
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-01-09 20:24:58 -0300

    primeiro commit

---
+++ src/main/java/br/com/wszd/notas/NotesStart.java
@@ -0,0 +1,13 @@
+package br.com.wszd.notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesStart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesStart.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wszd/notas/NotesStartTests.java
@@ -0,0 +1,13 @@
+package br.com.wszd.notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesStartTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

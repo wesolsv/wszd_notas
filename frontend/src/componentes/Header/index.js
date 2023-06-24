ace1dcff049b83a2a65f565e0af1ef3794a24eff
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-06-24 12:20:50 -0300

    incluindo header de teste

---
+++ frontend/src/componentes/Header/index.js
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h2>WeszDev</h2>
+        </div>
+    )
+}
+
+export default Header
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,10 +3,12 @@ import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
 import ListNotas from './pages/Notas';
 import Nota from './pages/Notas/Nota';
+import Header from './componentes/Header';
 
 const RoutesApp = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/"element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
